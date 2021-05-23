acfeeaa978cc960a67b72194be6b61745b16ed3c
MichaelKilbane <me@michaelkilbane.com>
2021-05-23 19:24:19 +1000

    Changed Env Configuration

--- api/src/databases/index.ts
+++ api/src/databases/index.ts
@@ -1,10 +1,13 @@
-import config from 'config';
-import { dbConfig } from '@interfaces/db.interface';
-
-const { username, password, host, port, database }: dbConfig = config.get('dbConfig');
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const database = process.env.DB_DB;
 
 export const dbConnection = {
-  url: `mongodb+srv://${username}:${password}@${host}${port ? `:${port}` : ''}/${database}`,
+  url: `mongodb+srv://${username}:${password}@${host}${
+    port ? `:${port}` : ''
+  }/${database}`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
